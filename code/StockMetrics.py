2a8b2562e73e8091b89df893652e122cdc4a779d
Bob <babur3000b@gmail.com>
2023-11-06 22:23:14 -0500

    added

--- code/StockMetrics.py
+++ code/StockMetrics.py
@@ -15,65 +15,64 @@ class StockMetrics(StockData):
 
     def average01(self):
         
-        averages = []
+        averages = [] #creating a list named averages where we will plant our mean. 
         for row in self.data:
-            values = []
+            values = [] #we create a list called value, here we will sort our values witout the firs column (date) 
             for val in row[0:]: 
-                try: 
-                    num = float(val)
-                    values.append(num)
+                try:  #we use Try and Except method because not all out values can be transfered to float so we skip the error. 
+                    num = float(val) #making sure that all values are float type
+                    values.append(num) #adding our new values to the values list from line 24
                 except ValueError:
                     pass
             if values: 
-                avg = round(sum(values)/len(values), 3)
-                averages.append(avg)  
-        return averages
+                avg = round(sum(values)/len(values), 3) #calculating mean by using new list values
+                averages.append(avg)  # putting this answer for average into our main list called averages 
+        return averages #everytime when function average01 will be used, the list called averages with mean for this date, will be used. 
 
       
     def median02(self):
         
-        medians = []
+        medians = [] #creating a list called medians 
         for row in self.data[0:]:
-            values = []
+            values = [] #creating list values
             for val in row: 
-                try: 
+                try: #using this concpet (ty and except) to avoid ValueError and format our data to a float format
                     num = float(val)
-                    values.append(num)
+                    values.append(num) #putting this new list of data into "values" list
                 except ValueError:
                     pass
             if values: 
-                data = sorted(values)
-                data1 = len(data)
-                middle = (data1 - 1) // 2
-                if data1 % 2 == 1: 
-                    median = round(data[middle], 6)
-                else:
+                data = sorted(values) #firt ordering the data in each row so it will go from lower to highest 
+                data1 = len(data) #figuring our how long is each row (like how many data in it)
+                middle = (data1 - 1) // 2 #figuring our the middle value 
+                if data1 % 2 == 1:  #we are using this if our row is odd 
+                    median = round(data[middle], 6) 
+                else: #using this if our data on the row is even 
                     median = round((data[middle] + data[middle + 1]) / 2, 6)
-                medians.append(median)
+                medians.append(median) #adding our answers in our main list medians
             else:
-                medians.append(0.0)
-        return medians
+                medians.append(0.0) 
+        return medians #the new values for medians will be returned when triggered
                 
 
     def stddev03(self):
-        """pt3
-        """
+        
         stdev3 = []
         for row in self.data[0:]:
-            values = []
+            values = [] #creating an empty list for tranformed data 
             for val in row: 
-                try: 
+                try:  #using try and excpet concept to filter the data and remove the first column (date)
                     num = float(val)
-                    values.append(num)
+                    values.append(num) #adding new data to the list created in line 62
                 except ValueError:
                     pass
-            if len(values):
-                n = len(values)
+            if len(values): #standart fromulat for finding the standart deviation 
+                n = len(values) 
                 mean = sum(values) / n
                 deviations = [(x - mean) ** 2 for x in values]
                 variance = sum(deviations) / (n - 1)
                 stdev = (variance ** 0.5)
-                stdev3.append(round(stdev, 3))
+                stdev3.append(round(stdev, 3))  #rounding and adding the stadart deviation for each row
             else: 
-                stdev3.append(0.0)
-        return stdev3
+                stdev3.append(0.0) 
+        return stdev3 #new values for stdev will be returned when trifgered
